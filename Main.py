7ff9f6ded086e35e9bb05bb3765b8f0256e2f413
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-17 12:31:03 +0800

    Pseudocode

--- Main.py
+++ Main.py
@@ -3,3 +3,56 @@
 # 07/17/2023
 # Covid Contact Tracing App
 
+# Pseudocode
+# Import Tkinter
+# Class for User Interface
+# Initilaize the frame
+# Method to Create Widget for the following:
+    # Introduce the Application
+    # 1. Personal Information
+    # Name:
+    # Age:
+    # Date of Birth:
+    # Gender:
+    # Nationality:
+    # Contact Number: 
+        # a) Mobile Number:
+        # b) Landline Number:
+    # Email Address:
+    # Current Home Address:
+    # 2. COVID-19 Diagnosis:
+    # Have you tested positive for COVID-19?
+    # If yes, when is the date of your positve test result?
+    # Have you been hospitalized due to COVID-19?
+    # Have you been sick of any of the following in the past 14 days?
+        # Fever - Yes/No
+        # Colds - Yes/No
+        # Cough - Yes/No
+        # Sore Throat - Yes/No
+        # Difficulty in Breating - Yes/No
+    # Close Contact Information
+    # Have you been in close contact with anyone who tested positive for COVID-19?
+    # If yes, Provide the name(s) and contact information of the person(s):
+    # When and where did you you have close contact with them?
+    # Recent Activities:
+    # Have you attended any gatherings or events recently? 
+    # If yes, provide details (date, location, nature of the event)
+    # Have you travelled recently?
+    # If yes, provide details (destinations, dates)
+    # Have you visited any high-risk settings such as healthcare facilities or crowded places?
+    # Have you been in close contact with vulnerable populations (elderly, immunocompromised individuals)?
+# Data Storage
+# Import JSON
+# Class for Person Details
+# Initialize Person Details
+# Method to save data to JSON file
+# Method to load data from JSON file
+# Method to search data form JSON file
+# Call the User Interface
+# Create the Window
+
+
+
+
+
+
