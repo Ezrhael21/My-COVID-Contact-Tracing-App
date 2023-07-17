76cb2352dec129d6162536c1fe168effda1ab06f
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-17 20:31:34 +0800

    Create a seperate class for User Personal Information

--- Application.py
+++ Application.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from Personal_Information import PersonalInfo
 
 # Create a class for user interface
 class UserInterface(tk.Tk):
@@ -7,3 +8,6 @@ class UserInterface(tk.Tk):
         super().__init__()
         self.title("ES COVID Contact Tracing App")
         self.geometry("800x500")
+        self.personal_info = PersonalInfo(self)
+
+    
