7086579f6550aa22ab473f6b54aa0b77135e8108
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-24 21:36:01 +0800

    Create a class for the introduction and create the widgets

--- Welcome.py
+++ Welcome.py
@@ -3,6 +3,15 @@ class Welcome(ctk.CTkFrame):
 	def __init__(self,parent):
 		super().__init__(parent)
 		self.pack(side = "top", expand = "True", fill = "both")
+		self.introduction()
+
+	def introduction(self):
+		title_label = ctk.CTkLabel(self, text = "Welcome to ES Covid Tracing App", font=("Helvetica", 30, "bold"))
+		title2_label = ctk.CTkLabel(self, text = "Your Reliable Contact Tracing Companion", font=("Helvetica", 20))
+		title3_label = ctk.CTkLabel(self, text ="Programmed by: Ezrhael R. Sicat", font=("Helvetica", 15))
 		
-	
+		title_label.place(x=50, y=100)
+		title2_label.place(x=50, y=150)
+		title3_label.place(x=50, y=300)
+		
 		
