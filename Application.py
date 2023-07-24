0f2ad378cab7169d38da01bcabd49a85baff3c65
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-24 19:38:49 +0800

    Create a new class file for Covid Diagnosis and create the widgets

--- Application.py
+++ Application.py
@@ -1,12 +1,14 @@
 import customtkinter as ctk
 from Personal_Info import PersonalInfo
+from Covid_Diagnosis import CovidDiagnosis
 
 # Create a class for user interface
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("ES COVID Contact Tracing App")
-        self.geometry("400x400")
+        self.geometry("600x600")
 
-        self.personal_info = PersonalInfo(self)
+        #self.personal_info = PersonalInfo(self)
+        self.covid_diagnosis = CovidDiagnosis(self)
     
