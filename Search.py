813e0905f5326808380e5f07bd85ececdc3c4bce
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-26 14:01:33 +0800

    Add Output Display and Search Button

--- Search.py
+++ Search.py
@@ -20,6 +20,28 @@ class Search(ctk.CTkFrame):
 		# Layout for Entry Widgets
 		self.name_entry.place(x=400, y=50)
 
+        # Output display for user input values
+		self.output_name = ctk.CTkLabel(self, text="Name:")
+		self.output_name.place(x=50, y=100)
+		
+		self.output_age = ctk.CTkLabel(self, text="Age:")
+		self.output_age.place(x=50, y=150)
+		
+		self.output_birthday = ctk.CTkLabel(self, text="Date of Birth:")
+		self.output_birthday.place(x=50, y=200)
+		
+		self.output_gender = ctk.CTkLabel(self, text="Gender:")
+		self.output_gender.place(x=50, y=250)
+
+		self.output_gender = ctk.CTkLabel(self, text="Contact Number:")
+		self.output_gender.place(x=50, y=300)
+
+		self.output_gender = ctk.CTkLabel(self, text="Email Address:")
+		self.output_gender.place(x=50, y=350)
+
+		self.output_gender = ctk.CTkLabel(self, text="Current Home Address:")
+		self.output_gender.place(x=50, y=400)	
+
 	
 
 
@@ -29,9 +51,11 @@ class Search(ctk.CTkFrame):
 		self.columnconfigure((0,1,), weight = 1)
 		self.rowconfigure((0,1,2,3,4,5), weight = 1)
 		back_button = ctk.CTkButton(self, text = "BACK", command = self.show_personal_info)
+		search_button = ctk.CTkButton(self, text = "SEARCH")
 		exit_button = ctk.CTkButton(self, text = "EXIT")
 		back_button.grid(row=5, column=0, padx=10, pady=10)
-		exit_button.grid(row=5, column=1, padx=10, pady=10)
+		search_button.grid(row=5, column=1, padx=10, pady=10)
+		exit_button.grid(row=5, column=2, padx=10, pady=10)
 	
 	# Method to redirect from current frame to Personal Info Frame
 	def show_personal_info(self):
