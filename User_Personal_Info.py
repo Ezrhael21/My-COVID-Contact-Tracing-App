7e69b53e0bf4754b314556febde67669f27f63e7
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-26 12:57:50 +0800

    Modify the code for data storage

--- User_Personal_Info.py
+++ User_Personal_Info.py
@@ -1,32 +1,42 @@
-import json
+#import json
+#file_path = "user_data.json"
+
 
 # Create a class for user data
-class UserData:
-    def __init__(self, name, age, birthday, gender, contacts, email, address, reference_number):
-        self.name = name
-        self.age = age
-        self.birthday = birthday
-        self.gender = gender
-        self.contacts = contacts
-        self.email = email
-        self.address = address
-        self.reference_number = reference_number
-
-    def to_json(self):
+#class UserData:
+#    def __init__(self, name, age, birthday, gender, contacts, email, address, reference_number):
+#        self.name = name
+#        self.age = age
+#        self.birthday = birthday
+#        self.gender = gender
+#        self.contacts = contacts
+#        self.email = email
+#        self.address = address
+#        self.reference_number = reference_number
+#
+#    def add_data(self, file_path):
         # Convert the UserData object to a dictionary
-        user_data_dict = {
-            "name": self.name,
-            "age": self.age,
-            "birthday": self.birthday,
-            "gender": self.gender,
-            "contacts": self.contacts,
-            "email": self.email,
-            "address": self.address,
-            "reference_number": self.reference_number
-        }
-        return json.dumps(user_data_dict, indent=4)
+#        user_data_dict = {
+#            "name": self.name,
+#            "age": self.age,
+#            "birthday": self.birthday,
+#            "gender": self.gender,
+#            "contacts": self.contacts,
+#            "email": self.email,
+#            "address": self.address,
+#            "reference_number": self.reference_number
+#       }
+#        try:
+#            # Read existing data from the JSON file
+#            with open(file_path, "r") as file:
+#                data = json.load(file)
+#        except FileNotFoundError:
+            # If the file does not exist, initialize an empty list
+ #           data = []
 
-    def save_to_json_file(self, file_path):
-        # Save the UserData object as a JSON file
-        with open(file_path, 'w') as file:
-            json.dump(self.to_json(), file)
+        # Append the new user data to the existing data
+ #       data.append(user_data_dict)
+
+        # Save the updated data to the JSON file
+  #      with open(file_path, "w") as file:
+   #         json.dump(data, file, indent=4)
